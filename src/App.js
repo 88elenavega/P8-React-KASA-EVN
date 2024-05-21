ba129d9fee5b895c66a2be8f6fa0ede1997372ba
Elena Vega Navarro <elenavn88@gmail.com>
2024-05-21 16:02:57 +0200

    Premiers composants

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Home } from './components/Home' 
+import { Header } from './components/Header' 
+import { Navbar } from './components/Navbar'
+
 
 function App() {
   return (
   <Router>
     <Routes>
       <Route path="/" element={<Home />} />
-     
+      <Route path='navbar' element={<Navbar />} />
+      <Route path='header' element={<Header />} />
     </Routes>
   </Router>
   );
---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+ 
+function Home() {
+    return (
+        <nav>
+            <Link to="/">Accueil</Link>
+            <Link to="/apropos">À Propos</Link>
+        </nav>
+    )
+}
+
+export default Home 
